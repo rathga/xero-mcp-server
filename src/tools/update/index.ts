36bb0f1c27317f05721b8382334026a8ad9f0ab2
Richard Davies <rathga@gmail.com>
2026-06-08 20:54:15 +0100

    feat: add update-repeating-invoice tool

--- src/tools/update/index.ts
+++ src/tools/update/index.ts
@@ -12,6 +12,7 @@ import UpdateManualJournalTool from "./update-manual-journal-tool.js";
 import UpdateQuoteTool from "./update-quote.tool.js";
 import UpdateTrackingCategoryTool from "./update-tracking-category.tool.js";
 import UpdateTrackingOptionsTool from "./update-tracking-options.tool.js";
+import UpdateRepeatingInvoiceTool from "./update-repeating-invoice.tool.js";
 
 export const UpdateTools = [
   UpdateContactTool,
@@ -26,5 +27,6 @@ export const UpdateTools = [
   UpdatePayrollTimesheetLineTool,
   RevertPayrollTimesheetTool,
   UpdateTrackingCategoryTool,
-  UpdateTrackingOptionsTool
+  UpdateTrackingOptionsTool,
+  UpdateRepeatingInvoiceTool
 ];
